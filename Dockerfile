e42c8f50d059d5a8a0d40be7a3b7a58ac4fb56d3
Johannes Heinecke <johannes.heinecke@orange.fr>
2021-01-02 21:10:58 +0100

    docker image

---
+++ Dockerfile
@@ -0,0 +1,37 @@
+FROM openjdk:8-alpine
+
+# docker build -t jheinecke/conllueditor:2.9.0 .
+# docker run -t --name conllueditor_290 -p 5555:5556 --user 1000:1000  -v </absolute/path/to/datadir>:/data  --env filename=tt.conllu jheinecke/conllueditor:latest
+# docker push jheinecke/conllueditor:2.9.0 .
+# docker push jheinecke/conllueditor:latest .
+
+# docker exec -it conllueditor_290 /bin/sh
+
+# stop and remove container docker rm --force conllueditor_290
+
+# https://ropenscilabs.github.io/r-docker-tutorial/04-Dockerhub.html
+
+RUN apk update && apk add  --no-cache bash
+
+WORKDIR /usr/src/ConlluEditor
+COPY target/ConlluEditor-2.9.0-jar-with-dependencies.jar ./ConlluEditor.jar
+
+COPY gui .
+COPY dockerstart.sh .
+#ENV CONLLUFILE=$filename
+
+#CMD echo "aa ${CONLLUFILE} bb $filename"
+
+#RUN ls -laR /
+#USER $uid:$gid
+
+#CMD java -Xmx4g -cp /usr/src/ConlluEditor/ConlluEditor-2.7.0-jar-with-dependencies.jar \
+#	com.orange.labs.editor.ConlluEditor \
+#	--rootdir /usr/src/ConlluEditor \
+#	/data/${filename}  5556
+WORKDIR /data
+CMD /usr/src/ConlluEditor/dockerstart.sh "$filename" "$validator" "$UPOS" "$XPOS" "$deprels" "$features" "$language" "${include_unused}" "$shortcuts" "$saveAfter" "$compare"
+
+
+
+
---
+++ dockerstart.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# echo "AAA $*"
+
+filename=$1
+shift
+
+if [ "$1" != "" ]; then
+    echo " ** cannot run validator within docker"
+    echo " ** please run it on the edited file ($1) outside docker"
+    #ivalidator="--validator $1"
+fi
+shift
+
+if [ "$1" != "" ]; then
+    iupos="--UPOS $1"
+fi
+shift
+
+if [ "$1" != "" ]; then
+    ixpos="--XPOS $1"
+fi
+shift
+
+if [ "$1" != "" ]; then
+    ideprels="--deprels $1"
+fi
+shift
+
+if [ "$1" != "" ]; then
+    ifeats="--features $1"
+fi
+shift
+
+if [ "$1" != "" ]; then
+    ilang="--language $1"
+fi
+shift
+
+if [ "$1" != "" ]; then
+    iunused="--include_unused"
+fi
+shift
+
+if [ "$1" != "" ]; then
+    ishortcuts="--shortcuts $1"
+fi
+shift
+
+if [ "$1" != "" ]; then
+    isaveafter="--saveAfter $1"
+fi
+shift
+
+if [ "$1" != "" ]; then
+    icompare="--compare $1"
+fi
+shift
+
+echo "options given:"
+echo "  validator      $ivalidator"
+echo "  upos           $iupos"
+echo "  xpos           $ixpos"
+echo "  deprels        $ideprels"
+echo "  features       $ifeats"
+echo "  language       $ilang"
+echo "  include unused $iunused"
+echo "  shortcuts      $ishortcuts"
+echo "  saveafter      $isaveafter"
+echo "  compare        $icompare"
+echo "  filename       $filename"
+
+java -Xmx4g -cp /usr/src/ConlluEditor/ConlluEditor.jar \
+	com.orange.labs.editor.ConlluEditor \
+	--rootdir /usr/src/ConlluEditor \
+        $ivalidator $iupos $ixpos $ideprels $ifeats $ilang $iunused $ishortcuts $isaveafter \
+        $icompare \
+	${filename}  5555
