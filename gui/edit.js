6912383e50be052972c19c25d0c01f46a65991bc
Johannes Heinecke <johannes.heinecke@orange.fr>
2025-07-05 21:56:25 +0200

    cleanup

--- gui/edit.js
+++ gui/edit.js
@@ -1827,7 +1827,6 @@ function formatPhrase(item) {
                     // TODO highlighting shift's if sum of tokens different to text
                     // so we do not show
                     console.log("III", item.errors);
-                    //if (!item.errors.incoherenttext) {
                     for (const start_end of item.textcheck) {
                         stext = stext.slice(0, start_end[1]) + "</span>" + stext.slice(start_end[1]);
                         var classes = "";
@@ -1838,14 +1837,7 @@ function formatPhrase(item) {
                             classes += " textcheck_deprel";
                         }
                         stext = stext.slice(0, start_end[0]) + '<span class="' + classes + '">' + stext.slice(start_end[0]);
-                        /*if (start_end[2] === 0) {
-                         stext = stext.slice(0, start_end[0]) + '<span class="textcheck">' + stext.slice(start_end[0]);
-                         } else {
-                         stext = stext.slice(0, start_end[0]) + '<span class="textcheck_deprel">' + stext.slice(start_end[0]);
-                         }*/
-                        //console.log("AAA", start_end[2], classes, start_end[2] & 1, start_end[2] & 2 ,stext);
                     }
-                    //}
                 } else {
                     // "# text = ...." does not correspond to concatenated tokens
                     stext = '<span class="textcheck">' + stext + "</span>";
